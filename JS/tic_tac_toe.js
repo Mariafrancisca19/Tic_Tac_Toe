c5a6dfb63e4c9c9f062f781a0b12c0c65df29dff
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-05-17 15:17:33 -0600

    Estructura del juego

---
+++ JS/tic_tac_toe.js
@@ -0,0 +1,12 @@
+/*let valor=document.getElementByIdClassName("cuadro")
+ 
+console.log("cuadro")
+
+cuadro[0].addEventListener("click", function () {
+    
+ cuadro[0].innetHTML="❌"
+    
+})*/
+
+
+
