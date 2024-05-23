9f8b287b442b58f7abbae09acce3db4ce8d1207e
Mariafrancisca19 <franciscasaborio19@gmail.com>
2024-05-23 15:31:01 -0600

    programacion de aleatoria

--- JS/tic_tac_toe.js
+++ JS/tic_tac_toe.js
@@ -11,30 +11,16 @@ let casilla9= document.getElementById("ocho")
 let arreglo=[casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8,casilla9]
 
 arreglo.forEach(element => element.addEventListener("click",function(){
-    element.innerHTML="❌"
-    juegoAleatorio()             
-}))
-
-
-/* posiciones ganadoras*/
- 
-const victoria= ()=>{
-    const posicionGanadora=
-
-        [0,1,2]
-        [3,4,5]
-        [6,7,8]
-        [0,3,6]
-        [1,4,7]
-        [2,5,8]
-        [0,4,8]
-        [2,4,6]
-
-
-
-
+if (element.innerHTML=="") {
+  
+  element.innerHTML="❌"
+  if(!victoria() && !empate()){
+  juegoAleatorio()
+  }else if(victoria()){}
+  else if(empate()){}         
 }
 
+}))
 
 
 
@@ -45,109 +31,56 @@ function juegoAleatorio() {
     casillasVacias[numRandom].innerHTML="⭕"
 }
 
-/*programacion de los botones de inicio , reiniciar la partida*/
-let iniciar=document.getElementById("inicio")    /*boton iniciar*/
-let reiniciar=document.getElementById("again")   /*boton reiniciar*/
+
+//guardar el ganador
+let ganador
+
+//posiciones ganadoras
+let quienGano
+function victoria(){
+  // Todas las posibles posiciones ganadoras
+  const posicionGanadoras=[
+      [0,1,2], //ESTO SON FILAS 
+      [3,4,5],
+      [6,7,8], // HASTA AQUI
+      [0,3,6], // ESTO SON COLUMNAS
+      [1,4,7],
+      [2,5,8], // HASTA AQUI
+      [0,4,8], // Diagonales
+      [2,4,6] // Diagonales
+  ]
+  // Un for para ir recorriendo cada posible ganador, en caso de haber uno devulve true 
+  for (let combinaciones of posicionGanadoras) {
+    [a,b,c] = combinaciones
+    if (arreglo[a].innerHTML && arreglo[a].innerHTML == arreglo[b].innerHTML && arreglo[a].innerHTML==arreglo[c].innerHTML) {
+      quienGano=arreglo[a].innerHTML
+      console.log(quienGano);
+      alert("GANADOR")
+      return true
+    }  
+  }
+  return false
+}
 
 
-iniciar.addEventListener("click", function (e) {
-  e.preventDefault
+/*EMPATE*/
+
+function empate() {
+  let nadieGano = arreglo.filter(nadieGano=>nadieGano.innerHTML==="")
+if (nadieGano.length==0) {
+  alert ("EMPATE")
+  return true
+}
+return false
+}
+
+
+/*programacion del boton reiniciar la partida*/
+
+let reiniciar = document.getElementById("again")   /*boton reiniciar*/
+
+reiniciar.addEventListener("click",function () {
+  location.reload()
 })
 
-
-reiniciar.addEventListener("click",function (e) {
-  e.preventDefault
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let cuadrados = document.querySelectorAll(".cuadro"); /*contenedor de todos las casillas*/
-//let turn="❌"
-//let finJuego=false
-
-
-//cuadrados.forEach(e =>{
-  //  e.innerHTML=" "
-    //e.addEventListener("click",()=> {
-      //  if (!finJuego && e.innerHTML ==="") {
-        //  e.innerHTML = turn;  
-    //    }
-   // })
-    
-
- //})
-
-
-
-/*const espaciosVacios = boxArray.filter(box => box.innerHTML === "");
-    const randomIndex = Math.floor(Math.random() * espaciosVacios.length);
-    espaciosVacios[randomIndex].innerHTML = "⭕";*/
-
-
-
-/*let flag=false
-                      profe
-while (!flag)
-  while(flag ==false)
-
-    */
-
-
-
-
-// let iniciar=document.getElementById("inicio")  /*boton iniciar*/
-// let volverajugar=document.getElementById("again")   /*reiniciar*/
-
-// let cuadrados = document.querySelectorAll(".cuadro"); /*contenedor de todos las casillas*/
-// const x = '❌'
-// const o = '⭕'
-// let turno = 'jugador1'
-
-// cuadrados.forEach((cuadros,i)=>{
-//     cuadros.addEventListener('click', () => {
-//         //console.log(cuadros,i)
-//     cuadros.innerText = turno === 'jugador1'? x : o;
-//     turno = turno === 'jugador1'? 'pancha': 'jugador1';
-//     console.log(cuadros)
-        
-
-//     })
-// })
-
-
-// inicio.addEventListener("click", function (e) {
-//     e.preventDefault
-// })
-// again.addEventListener("click",function (e) {
-//     e.preventDefault
-// })
-
-
-// function chequeo(a,b,c) {
-
-//     if (cuadro[a].innerHTML.length&&cuadro[a].innerHTML==cuadro[b].innerHTML&&cuadro[b].innerHTML==cuadro[c].innerHTML ) {
-//         alert("winner")
-//     }
-    
-// }
-
-
+ 
